5610caa4010edc75da74dd6ccf81ac34ebe29138
Mohan <mohansai2976@gmail.com>
2026-07-13 08:27:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("Site loaded successfully!");
-
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-    alert("Congratulations! 🎉\n\nYour website is running successfully on AWS S3 and can be deployed automatically using a CI/CD pipeline.");
-});
